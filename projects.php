8894d7f525977d67cd6be0f319ea353c1f43dc94
Eliane <eliane.perol@gmail.com>
2025-04-26 02:56:34 +0200

    minor fixes

--- projects.php
+++ projects.php
@@ -1363,7 +1363,7 @@ include_once "common/footer.php";
                         </div>
                         <div class="col-md-6">
                             <span class="">
-                                <a href="#" target="_blank"><i class="fa-solid fa-arrow-up-right-from-square"></i>      Extraits de code commentés</a>
+                                <a href="assets/docs/projects/welcome_training_extraits_code_commentes.pdf" target="_blank"><i class="fa-solid fa-arrow-up-right-from-square"></i>      Extraits de code commentés</a>
                             </span>
                         </div>
                         <div class="col-md-6">
